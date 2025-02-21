9569effd6493126c91c8bfdfae71daff5b7f687d
HwichulShin <shinhwiiron@gmail.com>
2025-02-21 18:07:27 +0900

    인트로 디자인 변경

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -5,13 +5,13 @@ import Logo from "@/components/Logo";
 function Intro() {
   return (
     <motion.div
-      className="fixed left-0 top-0 z-[3] flex h-screen w-full items-center justify-center bg-white"
+      className="fixed left-0 top-0 z-[3] flex h-screen w-full items-center justify-center bg-black"
       initial={{ opacity: 1 }}
       animate={{ opacity: 0 }}
       transition={{ delay: 1, duration: 1, ease: "easeOut" }} // ✅ 1.5초 동안 서서히 사라짐
     >
-      <div className="w-[250px] mo:w-[150px]">
-        <Logo type="col" />
+      <div className="w-[100px] mo:w-[80px]">
+        <Logo type="icon" />
       </div>
     </motion.div>
   );
