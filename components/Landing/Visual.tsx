5f2505a7c12836e68f99c960e378d94932e65766
HwichulShin <shinhwiiron@gmail.com>
2025-03-04 15:07:40 +0900

    Visual 영역 텍스트 애니메이션 하나만 노출되도록

--- components/Landing/Visual.tsx
+++ components/Landing/Visual.tsx
@@ -4,7 +4,7 @@ import VisualText from "@/components/Landing/VisualText";
 
 function Visual() {
   return (
-    <section className="relative h-[230vh] w-full">
+    <section className="relative size-full">
       <div className="sticky top-0 h-screen before:absolute before:z-[1] before:block before:size-full before:bg-black/50 before:content-['']">
         <video
           src="/video/video_main_1920.mp4"
@@ -29,38 +29,6 @@ function Visual() {
           <VisualText
             title="공간을 넘어, 새로운 가치를 경험"
             subtitle="혁신적인 솔루션으로 더 나은 미래를 만들어 갑니다."
-          />
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
-          transition={{
-            duration: 1,
-            delay: 0.5,
-          }}
-          className="mt-[10vh] flex h-[50vh] flex-col items-center justify-start"
-        >
-          <VisualText
-            title="공간을 기회로, 기회를 성공으로"
-            subtitle="성공적인 투자, 우리가 돕겠습니다."
-          />
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
-          transition={{
-            duration: 1,
-            delay: 0.5,
-          }}
-          className="mt-[10vh] flex h-[60vh] flex-col items-center justify-start"
-        >
-          <VisualText
-            title="모든 공간, 최고의 가치를 만나는 곳"
-            subtitle="더 높은 곳으로 임팩트코리아가 함께합니다."
           />
         </motion.div>
       </div>
