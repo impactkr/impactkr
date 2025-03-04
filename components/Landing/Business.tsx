5af82ac41194f3bcc760c679fafb576eff28d548
HwichulShin <shinhwiiron@gmail.com>
2025-03-04 15:55:28 +0900

    Business 반응형 작업

--- components/Landing/Business.tsx
+++ components/Landing/Business.tsx
@@ -1,13 +1,13 @@
 import { motion } from "framer-motion";
 
 import Inner from "@/components/Inner";
-import BusinessImg from "@/components/Landing/BusinessImg";
-import BusinessInfo from "@/components/Landing/BusinessInfo";
+
+import BusinessItem from "./BusinessItem";
 
 function Business() {
   return (
-    <section className="w-full mo:hidden">
-      <Inner className="flex justify-between py-[150px]">
+    <section className="w-full">
+      <Inner className="flex justify-between pt-[150px] ta:flex-col ta:py-[100px]">
         <motion.h3
           className="text-5xl font-medium tracking-tight text-[#555555]"
           initial={{ opacity: 0, y: 20 }}
@@ -21,64 +21,49 @@ function Business() {
           Business
         </motion.h3>
 
-        <ul className="flex justify-end gap-[50px]">
-          <motion.li
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{
-              duration: 1,
-              delay: 0.6,
-            }}
-          >
-            <BusinessImg src="/image/img_agency.jpg" />
+        <ul className="flex justify-end gap-[50px] ta:mt-[40px] ta:justify-between mo:flex-col">
+          <BusinessItem
+            delay={0.6}
+            src="/image/img_agency.jpg"
+            title="분양대행"
+            content={
+              <>
+                아파트, 오피스텔, 상업시설 등 다양한
+                <br className="ta:hidden" />
+                주거 및 비주거 상품의 맞춤형 분양
+                <br className="ta:hidden" />
+                전략 수립 및 실행
+              </>
+            }
+          />
 
-            <BusinessInfo title="분양대행">
-              아파트, 오피스텔, 상업시설, 지식산업센터 등
-              <br />
-              다양한 주거 및 비주거 상품의
-              <br />
-              맞춤형 분양 전략 수립 및 실행
-            </BusinessInfo>
-          </motion.li>
+          <BusinessItem
+            className="mt-[100px] ta:items-center"
+            delay={0.9}
+            src="/image/img_planning.jpg"
+            title="컨설팅"
+            content={
+              <>
+                고객 환경에 최적화된
+                <br className="ta:hidden" />
+                사업 기획, 설계, MD 구성 컨설팅 제공
+              </>
+            }
+          />
 
-          <motion.li
-            className="mt-[100px]"
-            initial={{ opacity: 0, y: 0 }}
-            whileInView={{ opacity: 1, y: 20 }}
-            viewport={{ once: true }}
-            transition={{
-              duration: 1,
-              delay: 0.9,
-            }}
-          >
-            <BusinessImg src="/image/img_planning.jpg" />
-
-            <BusinessInfo title="컨설팅">
-              고객 환경에 최적화된
-              <br />
-              사업 기획, 설계, MD 구성 컨설팅 제공
-            </BusinessInfo>
-          </motion.li>
-
-          <motion.li
-            className="mt-[50px]"
-            initial={{ opacity: 0, y: 0 }}
-            whileInView={{ opacity: 1, y: 20 }}
-            viewport={{ once: true }}
-            transition={{
-              duration: 1,
-              delay: 1.2,
-            }}
-          >
-            <BusinessImg src="/image/img_services.jpg" />
-
-            <BusinessInfo title="입주대행">
-              체계적인 입주 프로세스를 운영하여
-              <br />
-              고객과 입주자의 만족도를 극대화
-            </BusinessInfo>
-          </motion.li>
+          <BusinessItem
+            className="mt-[50px] ta:items-end"
+            delay={1.2}
+            src="/image/img_services.jpg"
+            title="입주대행"
+            content={
+              <>
+                체계적인 입주 프로세스를 운영하여
+                <br className="ta:hidden" />
+                고객과 입주자의 만족도를 극대화
+              </>
+            }
+          />
         </ul>
       </Inner>
     </section>
---
+++ components/Landing/BusinessItem.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import { ReactNode } from "react";
+import { motion } from "framer-motion";
+import classNames from "classnames";
+
+function BusinessItem({
+  className,
+  delay,
+  src,
+  title,
+  content,
+}: {
+  className?: string;
+  delay: number;
+  src: string;
+  title: string;
+  content: ReactNode;
+}) {
+  return (
+    <motion.li
+      className={classNames(
+        className,
+        "ta:mt-0 ta:flex ta:w-[30%] ta:flex-col mo:w-full",
+      )}
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{
+        duration: 1,
+        delay: delay,
+      }}
+    >
+      <div className="w-full">
+        <div className="relative h-[500px] w-[300px] ta:w-full mo:h-[300px]">
+          <Image src={src} alt="" fill className="object-cover" />
+        </div>
+
+        <dl className="mt-[30px] text-[#555555]">
+          <dt className="text-[32px] font-semibold">{title}</dt>
+          <dd className="mt-[16px] text-[18px] font-medium leading-[24px]">
+            {content}
+          </dd>
+        </dl>
+      </div>
+    </motion.li>
+  );
+}
+
+export default BusinessItem;
